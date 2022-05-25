6283a3529739338fafc65d80cd8e21cd077e7f24
meelosh9 <91650382+meelosh9@users.noreply.github.com>
2022-05-25 13:15:23 +0200

    Update handler.js

--- Seminarski/nodefn/nodefn/handler.js
+++ Seminarski/nodefn/nodefn/handler.js
@@ -2,8 +2,8 @@
 const SHA256  = require("crypto-js/sha256");
 module.exports = async (event, context) => {
   const result = {
-        'body': JSON.stringify(event.body) + SHA256(JSON.stringify(event.body)),
-    
+        'body': JSON.stringify(event.body),
+        'hash':SHA256(JSON.stringify(event.body))
   }
 
   return context
