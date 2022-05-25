c85690cea02fc8430d21afd79d352ed7191ac833
meelosh9 <91650382+meelosh9@users.noreply.github.com>
2022-05-25 12:18:34 +0200

    sha256

--- Seminarski/nodefn/nodefn/handler.js
+++ Seminarski/nodefn/nodefn/handler.js
@@ -1,8 +1,8 @@
 'use strict'
-
+const SHA256  = require("crypto-js/sha256");
 module.exports = async (event, context) => {
   const result = {
-    'body': JSON.stringify(event.body),
+    'body': JSON.stringify(event.body) + SHA256(JSON.stringify(event.body)),
     'content-type': event.headers["content-type"]
   }
 
